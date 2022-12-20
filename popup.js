c565742ed82c5ecaff56832a80b56d516943853d
Tanja Schmidt <tagretha@gmail.com>
2022-12-20 11:48:11 +0100

    added button/bg styles + fix notification on color click

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/* document.addEventListener("DOMContentLoaded", () => {
 
   const main = document.getElementById("main");
   const buttonCont = document.getElementById("picker-btn-cont");
@@ -9,15 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const tab = tabs[0]
 
     if (tab.url === undefined || tab.url.indexOf('chrome') === 0) {
-      buttonCont.innerHTML = '<span style="font-family: lobster, sans-serif">Eye Dropper</span> can\'t access <i>Chrome pages</i>'
+      buttonCont.innerHTML = '<span class="highlight">Lean Color Picker</span> can\'t access this <i>special Chrome page</i>. Please try a different web page.';
     }
     else if (tab.url.indexOf('file') === 0) {
-      buttonCont.innerHTML = '<span style="font-family: lobster, sans-serif">Eye Dropper</span> can\'t access <i>local pages</i>'
+      buttonCont.innerHTML = '<span class="highlight">Lean Color Picker</span> can\'t access <i>local pages</i>. Please try a different web page.';
 
     } else {
       const button = document.createElement("button")
       button.setAttribute("id", "picker-btn")
-      button.innerText = "Pick Color from webpage"
+      button.innerText = "Pick Color from Web Page"
 
       button.addEventListener("click", () => {
         if ('EyeDropper' in window) {
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
           window.close()
         } else {
           console.log('Your browser does not support the EyeDropper API');
-          return;
         }
       })
 
@@ -48,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         liElem.addEventListener("click", () => {
           navigator.clipboard.writeText(hexCode);
-          GiveMetheChild("#e19526", "Hex code is copied to clipboard!")
+         // alert("Hex code is copied to clipboard!")
         })
 
         resultList.appendChild(liElem)
@@ -56,7 +55,84 @@ document.addEventListener("DOMContentLoaded", () => {
       })
 
       const ClearButton = document.createElement("button")
-      ClearButton.innerText = "Clear Memory"
+      ClearButton.innerText = "Delete Color History"
+      ClearButton.setAttribute("id", "clearButton")
+
+      ClearButton.addEventListener("click", () => {
+        chrome.storage.local.remove("color_hex_code")
+        window.close()
+      })
+
+      main.appendChild(ClearButton)
+    }
+
+  })
+
+})
+*/
+document.addEventListener("DOMContentLoaded", () => {
+
+  const main = document.getElementById("main");
+  const buttonCont = document.getElementById("picker-btn-cont");
+  const resultList = document.getElementById("result");
+
+
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const tab = tabs[0]
+
+    if (tab.url === undefined || tab.url.indexOf('chrome') === 0) {
+      buttonCont.innerHTML = '<span class="highlight">Lean Color Picker</span> can\'t access this <i>special Chrome page</i>. Please try a different web page.';
+    }
+    else if (tab.url.indexOf('file') === 0) {
+      buttonCont.innerHTML = '<span class="highlight">Lean Color Picker</span> can\'t access <i>local pages</i>. Please try a different web page.';
+
+    } else {
+      const button = document.createElement("button")
+      button.setAttribute("id", "picker-btn")
+      button.innerText = "Pick Color from Web Page"
+
+      button.addEventListener("click", () => {
+        if ('EyeDropper' in window) {
+          chrome.tabs.sendMessage(
+              tabs[0].id,
+              {from: "popup", query: "eye_dropper_clicked"}
+          );
+          window.close()
+        } else {
+          console.log('Your browser does not support the EyeDropper API');
+        }
+      })
+
+      buttonCont.appendChild(button)
+    }
+  });
+
+  chrome.storage.local.get("color_hex_code", (resp) => {
+
+    if (resp.color_hex_code && resp.color_hex_code.length > 0) {
+
+      resp.color_hex_code.forEach(hexCode => {
+
+        const liElem = document.createElement("li")
+        liElem.innerText = hexCode
+        liElem.style.border = "solid 5px " + hexCode
+
+        liElem.addEventListener("click", () => {
+          navigator.clipboard.writeText(hexCode);
+          chrome.notifications.create({
+            type: "basic",
+            title: "Hex code copied",
+            message: "Hex code is copied to clipboard!",
+            iconUrl: "assets/icon128.png"
+          });
+        })
+
+        resultList.appendChild(liElem)
+
+      })
+
+      const ClearButton = document.createElement("button")
+      ClearButton.innerText = "Delete Color History"
       ClearButton.setAttribute("id", "clearButton")
 
       ClearButton.addEventListener("click", () => {
